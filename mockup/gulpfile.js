64ae149cfbc88ddc65e5fffbfe2e641796de9b51
Mark Goho <markgoho@gmail.com>
2017-01-31 20:23:37 -0500

    Adding mockup files

---
+++ mockup/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
+const autoprefix = require('gulp-autoprefixer');
+const del = require('del');
+const pug	= require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+const reload = browserSync.reload;
+
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  gulp.watch('*.html').on('change', reload);
+});
+
+gulp.task('default', ['deploy']);
