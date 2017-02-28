1e9461780b10b60374700c2e53c1abcbb77a1623
Mark Goho <markgoho@gmail.com>
2017-02-28 16:11:46 -0500

    Deploying to firebase, adding contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-contact',
   styleUrls: ['./contact.component.scss'],
   template: `
-
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeiNfQQFbqNAGwtaStGqDRn7FqVxrfUADx77dWtq4UkkTypUg/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
   `
 })
 export class ContactComponent implements OnInit {
